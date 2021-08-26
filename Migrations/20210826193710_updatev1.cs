506b293eddff2b2d8305be9725d991a74664b1e5
Gabriel Alves Moura <Gbrextreme@hotmail.com>
2021-08-26 16:37:24 -0300

    update database to version 1

---
+++ Migrations/20210826193710_updatev1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OrderAPI.Migrations
+{
+    public partial class updatev1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "Usuario",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "token",
+                table: "Funcionario",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "Usuario");
+
+            migrationBuilder.DropColumn(
+                name: "token",
+                table: "Funcionario");
+        }
+    }
+}
