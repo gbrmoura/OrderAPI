217c80aa46d46791c2978a8da1d74139bab742ef
Gabriel Alves Moura <Gbrextreme@hotmail.com>
2021-08-30 16:54:29 -0300

    Inserindo logs personalizados usando a lib Serilog

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,11 +11,27 @@ using System.Threading.Tasks;
 namespace OrderAPI {
     public class Program {
         public static void Main(string[] args) {
-            CreateHostBuilder(args).Build().Run();
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try {
+                Log.Information("Starting the server...");
+                CreateHostBuilder(args).Build().Run();
+            } catch (Exception e) {
+                Log.Fatal(e, "Fatal error occurred");
+            } finally {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder => {
                     webBuilder.UseStartup<Startup>();
                 });
