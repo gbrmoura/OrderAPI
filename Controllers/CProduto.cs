1472fe019dd4fab3d57f7a89f3902f8c72bae158
Gabriel Alves Moura <Gbrextreme@hotmail.com>
2021-09-17 11:28:51 -0300

    rota produto atualizado

--- Controllers/CProduto.cs
+++ Controllers/CProduto.cs
@@ -10,6 +10,8 @@ using OrderAPI.Models;
 using OrderAPI.Services;
 using OrderAPI.Data.Request;
 using OrderAPI.Data.Response;
+using OrderAPI;
+
 namespace orderapi.Controllers {
     
     [Route("api/produto/")]
@@ -21,6 +23,56 @@ namespace orderapi.Controllers {
         public CProduto(DBContext context, IMapper mapper) {
             this._context = context;
             this._mapper = mapper;
+        }
+
+        [HttpPost("registrar")]
+        [Authorize(Roles = "")]
+        public ActionResult<HttpResponse> Registrar([FromBody] CriarProdutoRequest dados) {
+            HttpResponse response = new HttpResponse() {
+                Code = (int)EHttpResponse.UNAUTHORIZED,
+                Message = "Rota não autorizada"
+            };
+
+            if (!ModelState.IsValid) {
+                response.Message = "Parametros Ausentes";
+                response.Error = ModelStateService.ErrorConverter(ModelState);
+                return StatusCode(response.Code, response);
+            }
+
+            try {
+                
+                MCategoria categoria = _context.Categoria
+                    .FirstOrDefault((element) => element.Codigo == dados.Categoria);
+
+                if (categoria == null) {
+                    response.Message = "Categoria não encontrada";
+                    return StatusCode(response.Code, response);
+                }
+
+                MProduto produto = _context.Produto
+                    .FirstOrDefault((element) => element.Titulo.Equals(dados.Titulo) && element.Status == true);
+
+                if (produto != null) {
+                    response.Message = "Produto ja cadastrado";
+                    return StatusCode(response.Code, response);
+                }
+
+                MProduto produtoDB = _mapper.Map<MProduto>(dados);
+                produtoDB.Status = true;
+
+                _context.Produto.Add(produtoDB);
+                _context.SaveChanges();
+
+                response.Code = (int)EHttpResponse.OK;
+                response.Message = "Produto cadastrado com sucesso";
+                return StatusCode(response.Code, response);
+
+            } catch (Exception E) {
+                response.Code = (int)EHttpResponse.INTERNAL_SERVER_ERROR;
+                response.Message = "Erro interno do servidor!";
+                response.Error = E.Message;
+                return StatusCode(response.Code, response);
+            }
         }
 
     }
--- Data/Request/CriarProdutoRequest.cs
+++ Data/Request/CriarProdutoRequest.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+using OrderAPI.Enums;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace OrderAPI.Models {
-    public class MProduto {
-        [Key]
-        public int Codigo { get; set; }
+namespace OrderAPI.Data.Request {
 
+    public class CriarProdutoRequest {
         [Required(ErrorMessage = "Titulo deve ser informado.")]
         [MaxLength(45,  ErrorMessage = "O limite de 45 caractéres foi atingido.")]
         public string Titulo { get; set; }
@@ -16,13 +11,9 @@ namespace OrderAPI.Models {
         [MaxLength(245, ErrorMessage = "O limite de 245 caractéres foi atingido.")]
         public string Descricao { get; set; }
 
-        [Required(ErrorMessage = "Quantidade deve ser informado.")]
-        public int Quantidade { get; set; }
-
         [Required(ErrorMessage = "Valor deve ser informado.")]
         public float Valor { get; set; }
 
-        public Boolean Status { get; set; }
-        public MCategoria Categoria { get; set; }
+        public int Categoria { get; set; }
     }
 }
