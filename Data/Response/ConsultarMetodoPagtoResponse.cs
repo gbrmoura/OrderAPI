9ef2b6e207b8087d0ab250b4491a8030d88f9d5d
Arthur Machado <arthur.machado15@hotmail.com>
2021-09-21 16:16:07 -0300

    Consultar e Alterar Requests

---
+++ Data/Response/ConsultarMetodoPagtoResponse.cs
@@ -0,0 +1,14 @@
+using OrderAPI.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderAPI.Data.Response {
+
+    public class ConsultarMetodoPagtoResponse {
+        [Required(ErrorMessage = "Nome deve ser informado.")]
+        [MaxLength(45, ErrorMessage = "O limite de 45 caractéres foi atigido.")]
+        public string Nome { get; set; }
+        
+    }
+}
