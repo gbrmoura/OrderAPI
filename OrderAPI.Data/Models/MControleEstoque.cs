905cd69f66940ec111c1ea94a3dc2e6f751842d5
gbrextreme <gbrextreme@hotmail.com>
2021-10-15 11:46:24 -0300

    Alteração de Projeto

---
+++ OrderAPI.Data/Models/MControleEstoque.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderAPI.Data.Models 
+{
+    public class MControleEstoque 
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Quantidade deve ser informada.")]
+        public int Quantidade { get; set; }
+
+        [MaxLength(245, ErrorMessage = "O limite de 245 caractéres foi atingido.")]
+        public string observacao { get; set; }
+    }
+}
---
+++ OrderAPI.Data/Models/MPedido.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OrderAPI.Data.Models 
+{
+    public class MPedido 
+    {
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Data deve ser informada.")]
+        public DateTime Data { get; set; }
+
+        [Required(ErrorMessage = "Status deve ser informado.")]
+
+        [MaxLength(245, ErrorMessage = "O limite de 245 caractéres foi atingido.")]
+        public string Observacao { get; set; }
+
+        public List<MPedidoItem> Items { get; set; }
+        
+        [ForeignKey("MetodoPagamento")]
+        public int MetodoPagamentoCodigo { get; set; }
+
+        public MMetodoPagamento MetodoPagamento { get; set; }
+
+        private bool _status = true;
+        public bool Status {
+            get { return _status; }
+            set { _status = value; }
+        }
+    }
+}
