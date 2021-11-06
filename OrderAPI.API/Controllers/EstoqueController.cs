91d91b6fabe5e8c5e28865f72ee8ea824fc49b9c
gbrextreme <gbrextreme@hotmail.com>
2021-11-06 17:44:11 -0300

    ..

--- OrderAPI.API/Controllers/EstoqueController.cs
+++ OrderAPI.API/Controllers/EstoqueController.cs
@@ -53,7 +53,50 @@ namespace OrderAPI.API.Controllers
                     return StatusCode(response.Code, response);
                 }
                 
-                produto.Quantidade = body.Quantidade;
+                produto.Quantidade += body.Quantidade;
+                _context.SaveChanges();
+
+                response.Code = StatusCodes.Status200OK;
+                response.Message = "Estoque registrado com sucesso.";
+                return StatusCode(response.Code, response);
+            }
+            catch (Exception err)
+            {
+                response.Code = StatusCodes.Status500InternalServerError;
+                response.Message = "Erro ao registrar produto estoque.";
+                response.Error = err.Message;
+                return StatusCode(response.Code, response);
+            }
+        }
+
+        [HttpPost("Retirar/")]
+        [Authorize(Roles = "MASTER, GERENTE, FUNCIONARIO")]
+        public ActionResult<DefaultResponse> Retirar([FromBody] RegistrarEstoqueRequest body)
+        {
+            DefaultResponse response = new DefaultResponse()
+            {
+                Code = StatusCodes.Status401Unauthorized,
+                Message = "Rota não autorizada."
+            };
+
+            if (!ModelState.IsValid) 
+            {
+                response.Message = "Parametros Ausentes.";
+                response.Error = ModelStateService.ErrorConverter(ModelState);
+                return StatusCode(response.Code, response);
+            }
+
+            try
+            {
+                var produto = _context.Produto.SingleOrDefault(x => x.Codigo == body.ProdutoCodigo);
+
+                if (produto == null) 
+                {
+                    response.Message = "Produto não encontrado.";
+                    return StatusCode(response.Code, response);
+                }
+                
+                produto.Quantidade -= body.Quantidade;
                 _context.SaveChanges();
 
                 response.Code = StatusCodes.Status200OK;
