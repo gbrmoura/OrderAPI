e728989719516639af7c4701d8d01d199b647ed4
Gabriel Alves <gbrextreme@hotmail.com>
2022-02-11 19:48:35 -0300

    Atualização do banco de dados

--- OrderAPI.Data/Migrations/20220211223549_FuncionarioEmail.Designer.cs
+++ OrderAPI.Data/Migrations/20220211223549_FuncionarioEmail.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using OrderAPI.Data;
 
 namespace OrderAPI.Data.Migrations
 {
     [DbContext(typeof(OrderAPIContext))]
-    partial class OrderAPIContextModelSnapshot : ModelSnapshot
+    [Migration("20220211223549_FuncionarioEmail")]
+    partial class FuncionarioEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
