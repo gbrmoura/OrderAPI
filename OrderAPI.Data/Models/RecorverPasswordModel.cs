40df375946cfbe226a9aa3ff98fd68c1b481c15c
Gabriel Alves <gbrextreme@hotmail.com>
2022-02-14 21:20:39 -0300

    versão de banco de dados

---
+++ OrderAPI.Data/Models/RecorverPasswordModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace OrderAPI.Data.Models
+{
+    public class RecorverPasswordModel
+    { 
+        [Key]
+        public int Codigo { get; set; }
+
+        [Required(ErrorMessage = "Email deve ser informado.")]
+        [EmailAddress(ErrorMessage = "Email inválido.")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Token de validação deve ser informado.")]
+        [MaxLength(8, ErrorMessage = "O limite de 8 caractéres foi atingido.")]
+        public string Token { get; set; }
+    }
+}
